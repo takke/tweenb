34f6b3232dac71b8a7cfc7e2f6093b4bca5bfa1e
takke <takke30@gmail.com>
2025-03-07 10:41:13 +0900

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+@Preview
+fun AppScreen() {
+  var text by remember { mutableStateOf("Hello, World!") }
+
+  MaterialTheme {
+    Button(onClick = {
+      text = "Hello, Desktop!"
+    }) {
+      Text(
+        text = text,
+        style = MaterialTheme.typography.body1
+      )
+    }
+  }
+}
+
+fun main() = application {
+  Window(
+    onCloseRequest = ::exitApplication,
+    title = "demo of CMP"
+  ) {
+    AppScreen()
+  }
+}
