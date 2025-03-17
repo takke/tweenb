02a21eacbb4c4e2cc9232570cf5c96a4f2d03c62
takke <takke30@gmail.com>
2025-03-17 11:14:13 +0900

    appモジュール作成

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.compose)
+  alias(libs.plugins.kotlin.compose)
+}
+
+group = "jp.takke.tweenb"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+  // Note, if you develop a library, you should use compose.desktop.common.
+  // compose.desktop.currentOs should be used in launcher-sourceSet
+  // (in a separate module for demo project and in testMain).
+  // With compose.desktop.common you will also lose @Preview functionality
+  implementation(compose.desktop.currentOs)
+
+  // JetBrains Compose
+  implementation(libs.compose.ui.ui)
+  implementation(libs.compose.ui.tooling)
+  implementation(libs.compose.material.material)
+  implementation(libs.compose.runtime)
+}
+
+compose.desktop {
+  application {
+    mainClass = "jp.takke.tweenb.MainKt"
+
+    nativeDistributions {
+      targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+      packageName = "tweenb"
+      packageVersion = "1.0.0"
+    }
+  }
+} 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "tweenb"
+include("app")
