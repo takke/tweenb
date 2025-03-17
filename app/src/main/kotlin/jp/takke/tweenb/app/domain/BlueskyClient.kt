659ca20e106d5a2880e984785607ab5e4a8a01f7
takke <takke30@gmail.com>
2025-03-17 20:43:05 +0900

    executeWithAutoRefresh

--- app/src/main/kotlin/jp/takke/tweenb/app/domain/BlueskyClient.kt
+++ app/src/main/kotlin/jp/takke/tweenb/app/domain/BlueskyClient.kt
@@ -1,14 +1,17 @@
 package jp.takke.tweenb.app.domain
 
 import jp.takke.tweenb.app.AppConstants
+import jp.takke.tweenb.app.repository.AppPropertyRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import work.socialhub.kbsky.Bluesky
 import work.socialhub.kbsky.BlueskyFactory
 import work.socialhub.kbsky.api.entity.app.bsky.feed.FeedGetTimelineRequest
+import work.socialhub.kbsky.auth.AuthFactory
+import work.socialhub.kbsky.auth.OAuthContext
 import work.socialhub.kbsky.auth.OAuthProvider
-import java.text.SimpleDateFormat
-import java.util.*
+import work.socialhub.kbsky.auth.api.entity.oauth.OAuthRefreshTokenRequest
+import java.lang.String.format
 
 /**
  * Blueskyクライアントのインターフェース
@@ -80,8 +83,8 @@ private class BlueskyClientImpl : BlueskyClient {
   /**
    * OAuthContextを作成する
    */
-  private fun createOAuthContext(account: Account): work.socialhub.kbsky.auth.OAuthContext {
-    return work.socialhub.kbsky.auth.OAuthContext().also {
+  private fun createOAuthContext(account: Account): OAuthContext {
+    return OAuthContext().also {
       it.clientId = AppConstants.OAUTH_CLIENT_ID
       it.dPoPNonce = account.dPoPNonce
       it.publicKey = account.publicKey
@@ -102,7 +105,9 @@ private class BlueskyClientImpl : BlueskyClient {
         val request = FeedGetTimelineRequest(authProvider!!)
         request.limit = limit
 
-        val response = factory.feed().getTimeline(request)
+        val response = executeWithAutoRefresh { bluesky ->
+          bluesky.feed().getTimeline(request)
+        }
 
         // レスポンスから投稿のリストを取得し、BsFeedViewPostに変換
         response.data.feed
@@ -114,17 +119,108 @@ private class BlueskyClientImpl : BlueskyClient {
   }
 
   /**
-   * 日付文字列をDateオブジェクトに変換する
+   * Bluesky の API を呼び出す
+   *
+   * トークンの自動リフレッシュ＆保存も行う
    */
-  private fun parseDate(dateStr: String): Date {
-    return try {
-      // ISO 8601形式の日付文字列をパース
-      val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
-      format.timeZone = TimeZone.getTimeZone("UTC")
-      format.parse(dateStr) ?: Date()
+  suspend fun <T> executeWithAutoRefresh(
+    request: suspend (Bluesky) -> T
+  ): T {
+
+    //--------------------------------------------------
+    // exp が近い場合はリフレッシュする
+    //--------------------------------------------------
+    val exp = authProvider!!.jwt.exp
+    val now = System.currentTimeMillis() / 1000
+    val remainSec = exp - now
+    if (remainSec < 60) {
+      println("exp が近いので refresh する: remain[${remainSec}s=${secToHMS(remainSec)}]")
+
+      // トークンリフレッシュは直列化する
+      val appPropertyRepository = AppPropertyRepository.instance
+      val updatedAccount = refreshTokenForOAuth(appPropertyRepository)
+      initialize(updatedAccount)
+    }
+
+    try {
+      //--------------------------------------------------
+      // リクエスト実行
+      //--------------------------------------------------
+      return request.invoke(factory).also {
+
+        // dPoPNonce を保存する
+        // TODO 実装すること
+//        bskyAccountProvider.updateBlueskyDPoPNonce(accountIdWIN, client.oAuthSession?.dPoPNonce)
+      }
     } catch (e: Exception) {
+
+      println("API呼び出しエラー: ${e.message}")
       e.printStackTrace()
-      Date()
+      throw e
     }
   }
-} 
+
+  private fun secToHMS(remainSec0: Long): String {
+    val remainSec = if (remainSec0 < 0) -remainSec0 else remainSec0
+    val minusFlag = if (remainSec0 < 0) "-" else ""
+
+    val day = remainSec / (60 * 60 * 24)
+    val hour = (remainSec % (60 * 60 * 24)) / (60 * 60)
+    val min = (remainSec % (60 * 60)) / 60
+    val sec = remainSec % 60
+
+    return minusFlag + if (day > 0) {
+      format("%dd %02d:%02d:%02d", day, hour, min, sec)
+    } else {
+      format("%02d:%02d:%02d", hour, min, sec)
+    }
+  }
+
+  /**
+   * OAuth 方式のトークンリフレッシュ
+   */
+  @Synchronized
+  private fun refreshTokenForOAuth(appPropertyRepository: AppPropertyRepository): Account {
+
+    // 同時にリフレッシュした場合、Synchronized で排他制御されているため同時実行はされないが、
+    // 直前のリフレッシュで呼び出し元の client はリフレッシュトークンが書き換わっている可能性があるため、ここで client を再取得する
+    val account = account ?: throw IllegalStateException("BlueskyClient が初期化されていません")
+
+    val oAuthContext = OAuthContext().also {
+      it.clientId = AppConstants.OAUTH_CLIENT_ID
+      it.redirectUri = AppConstants.CALLBACK_URL
+      it.publicKey = account.publicKey
+      it.privateKey = account.privateKey
+      it.dPoPNonce = account.dPoPNonce
+    }
+
+    val authProvider = authProvider ?: throw IllegalStateException("OAuthProvider が初期化されていません")
+    println("OAuth refresh 開始: dPoPNonce[${oAuthContext.dPoPNonce}], refreshToken[${authProvider.refreshTokenJwt}]")
+    val response = AuthFactory
+      .instance(authProvider.pdsEndpoint)
+      .oauth()
+      .refreshTokenRequest(
+        oAuthContext,
+        OAuthRefreshTokenRequest(authProvider)
+      )
+
+    val rr = response.data
+    println("refresh response: $rr")
+
+    // Refresh したトークンを保存する
+    val updatedAccount = Account(
+      accountId = account.accountId,
+      screenName = account.screenName,
+      accessJwt = rr.accessToken,
+      refreshJwt = rr.refreshToken,
+      dPoPNonce = oAuthContext.dPoPNonce,
+      publicKey = oAuthContext.publicKey ?: "",
+      privateKey = oAuthContext.privateKey ?: "",
+    )
+    // アカウント情報を永続化
+    appPropertyRepository.saveAccount(account)
+    println("refresh 完了: updated[$updatedAccount]")
+
+    return updatedAccount
+  }
+}
