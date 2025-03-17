269340ab84763bee4ab5f060377ca41b4b395595
takke <takke30@gmail.com>
2025-03-17 10:58:36 +0900

    Update build configuration to use version catalogs for dependencies and plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,13 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-  kotlin("jvm")
-  id("org.jetbrains.compose")
-  id("org.jetbrains.kotlin.plugin.compose")
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.compose)
+  alias(libs.plugins.kotlin.compose)
 }
 
 group = "jp.takke.tweenb"
 version = "1.0-SNAPSHOT"
-
-repositories {
-  mavenCentral()
-  maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-  google()
-}
 
 dependencies {
   // Note, if you develop a library, you should use compose.desktop.common.
@@ -21,6 +15,12 @@ dependencies {
   // (in a separate module for demo project and in testMain).
   // With compose.desktop.common you will also lose @Preview functionality
   implementation(compose.desktop.currentOs)
+
+  // JetBrains Compose
+  implementation(libs.compose.ui.ui)
+  implementation(libs.compose.ui.tooling)
+  implementation(libs.compose.material.material)
+  implementation(libs.compose.runtime)
 }
 
 compose.desktop {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,14 @@ pluginManagement {
     gradlePluginPortal()
     mavenCentral()
   }
+}
 
-  plugins {
-    kotlin("jvm").version(extra["kotlin.version"] as String)
-    id("org.jetbrains.compose").version(extra["compose.version"] as String)
-    id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+    mavenCentral()
   }
 }
 
