dc20a5bbe10d656883e426f66f9515356d074a2e
takke <takke30@gmail.com>
2025-03-18 15:14:12 +0900

    URL変更

--- app/src/main/kotlin/jp/takke/tweenb/app/AppConstants.kt
+++ app/src/main/kotlin/jp/takke/tweenb/app/AppConstants.kt
@@ -3,8 +3,8 @@ package jp.takke.tweenb.app
 // アプリケーション全体で使用する定数
 object AppConstants {
 
-  const val OAUTH_CLIENT_ID: String = "https://twitpane.com/oauth/bluesky/twitpane/client-metadata.json"
-  const val CALLBACK_URL: String = "https://twitpane.com/oauth/bluesky/twitpane/callback"
+  const val OAUTH_CLIENT_ID: String = "https://twitpane.com/oauth/bluesky/tweenb/client-metadata.json"
+  const val CALLBACK_URL: String = "https://twitpane.com/oauth/bluesky/tweenb/callback"
 
   // 自動更新間隔（秒）
   val AUTO_REFRESH_INTERVALS = listOf(60, 90, 120, 300, 600)
